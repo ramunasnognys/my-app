49a1419e65247903c4b45ad866479288dbbf6678
ramunasnognys <dev.nognys@gmail.com>
2024-10-03 11:15:47 +0200

    Initial commit

---
+++ app/components/RamasCode.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RamasCode: React.FC = () => {
+  return (
+    <div className="ramas-code font-mono">
+      ramas code
+    </div>
+  );
+};
+
+export default RamasCode;
